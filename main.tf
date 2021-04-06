9a6d81c5824982ead4e76c95f978fb9ccd73fbb1
simranhussain <shineim@gmail.com>
2021-04-05 23:48:04 -0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,28 @@
+
+
+
+resource "aws_instance" "myec2" {
+
+  count = var.instancecount
+
+  ami           = var.amiid
+  instance_type = "t2.micro"
+
+tags = {
+    "Name" = "Imran-instance-${count.index + 1}"
+  }
+}
+
+
+output "myec2instanceipaddress" {
+
+  value = aws_instance.myec2[*].public_ip
+
+}
+
+
+output "myec2instanceprivateipaddress" {
+
+  value = aws_instance.myec2[*].private_ip
+
+}
